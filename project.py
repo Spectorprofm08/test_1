f2e63a3cab94b3658b90eb585669ba9191b8baed
Spectorprofm08 <127282272+Spectorprofm08@users.noreply.github.com>
2026-03-06 16:21:30 +0500

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,8 @@ class  Character(object):
     def __init__(self):
         self.mana = 150
         self.hp = 100
-class Character_1(Character):
+# Vector
+class Character_vect(Character):
     def __init__(self,mana, hp):
         self.hp = hp
         self.mana = mana
@@ -21,7 +22,8 @@ class Character_1(Character):
                         return self.mana
         else:
             return self.mana
-class Character_2(Character):
+# Virus
+class Character_vir(Character):
     def __init__(self,mana, hp):
         self.hp = hp
         self.mana = mana
@@ -38,25 +40,70 @@ class Character_2(Character):
                         return self.mana
         else:
             return self.mana
-fin = Character_1(100, 100)
-vir = Character_2(120, 100)
+# Finik
+class Character_fin(Character):
+    def __init__(self, mana, hp):
+        self.hp = hp
+        self.mana = mana
+        self.Type_element_spell = "potion" 
+        self.spell = ["mana_potion", "splash_ragen_potion", "ragen_potion"]
+        self.count_mane_of_spell = [10, 15, 20]
+        self.regen_of_spell = [25, 40, 50]
+    def cast(self, spell, character):
+        if (random.randint(0,2) == 1) and (self.mana > 0):
+            for n in range(len(self.spell)):
+                if (self.spell[n] == spell) and (spell == "mana_potion"):
+                        self.mana = self.mana - self.count_mane_of_spell[n]
+                        character.mana = character.mana + self.regen_of_spell[n]
+                        return self.mana
+                if (self.spell[n] == spell) and (spell == "splash_ragen_potion"):
+                    self.mana = self.mana - self.count_mane_of_spell[n]
+                    vect.hp = vect.hp + self.regen_of_spell[n]
+                    vir.hp = vir.hp + self.regen_of_spell[n]
+                    fin.hp = fin.hp + self.regen_of_spell[n]
+                    return self.mana
+                if (self.spell[n] == spell) and (spell == "ragen_potion"):
+                    self.mana = self.mana - self.count_mane_of_spell[n]
+                    character.hp = character.hp + self.regen_of_spell[n]
+                    return self.mana
+        else:
+            return self.mana
+# настройка маны и здоровья (mana, hp)
+vect = Character_vect(100, 100)
+vir = Character_vir(120, 100)
+fin = Character_fin(150, 100)
+
+# print("mana_potion", fin.cast("mana_potion", vir), ":fin mana", "=>", vir.mana, ":vir Mana")
+# print("splash_ragen_potion", fin.cast("splash_ragen_potion", fin), ":fin mana", "=>", vect.hp, ":vect HP", vir.hp, ":vir HP", fin.hp, ":fin HP")
+# print("ragen_potion", fin.cast("ragen_potion", vect), ":fin mana", "=>", vect.hp, ":vect HP")
+# # жерибьевка хода
 attack = 0
-while (vir.hp > 0) and (fin.hp > 0) and ((fin.mana > 0) or (vir.mana > 0)):
-    fin_attack = random.choice(fin.spell)
-    vir_attack = random.choice(vir.spell)
-    if (random.randint(0,1) == 1):
+pl_vect_move = [vir,fin]
+pl_vir_move = [vect,fin]
+pl_fin_move = [vect,vir,fin]
+while (vir.hp > 0) and (vect.hp > 0) and ((vect.mana > 0) or (vir.mana > 0)):
+    move  = random.randint(0,2)
+    if (move == 0):
         attack = attack + 1
-        print("ход", attack, "Fin")
-        print(fin_attack, fin.cast(fin_attack, vir), ":fin mana", ",", vir.hp, ":vir HP")
-        print(vir_attack, vir.cast(vir_attack, fin), ":vir mana", ",", fin.hp, ":fin HP")
-    else:
+        print("ход", attack, "Vector")
+        vect_attack = random.choice(vect.spell)
+        print(vect_attack, vect.cast(vect_attack, pl_vect_move), ":vect mana", "=>", pl_vect_move.hp, ":",pl_vect_move, "HP")
+    elif (move == 1):
         attack = attack + 1
-        print("ход", attack, "Vir")
-        print(vir_attack, vir.cast(vir_attack, fin), ":vir mana", ",", fin.hp, ":fin HP")
-        print(fin_attack, fin.cast(fin_attack, vir), ":fin mana", ",", vir.hp, ":vir HP")
-if (fin.hp <= 0):
-    print("Выйграл - Vir", "(за", attack, "ходов)")
+        print("ход", attack, "Virus")
+        vir_attack = random.choice(vir.spell)
+        print(vir_attack, vect.cast(vir_attack, pl_vir_move), ":vir mana", "=>", pl_vir_move.hp, ":",pl_vir_move, "HP")
+    elif (move == 2):
+        attack = attack + 1
+        print("ход", attack, "Finik")
+        fin_attack = random.choice(fin.spell)
+        if fin_attack != "splash_ragen_potion":
+            print(fin_attack, fin.cast(fin_attack, pl_fin_move), ":fin mana", "=>", pl_fin_move.hp, ":",pl_fin_move, "HP")
+        else:
+            print(fin_attack, fin.cast(fin_attack, fin), ":fin mana", "=>", vect.hp, ":vect HP", vir.hp, ":vir HP", fin.hp, ":fin HP")
+if (vect.hp <= 0):
+    print("Выйграл - Virus", "(за", attack, "ходов)")
 elif (vir.hp <= 0):
-    print("Выйграл - Fin", "(за", attack, "ходов)")
+    print("Выйграл - Vector", "(за", attack, "ходов)")
 else:
     print("Ничья")
